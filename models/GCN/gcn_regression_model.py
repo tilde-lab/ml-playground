a0c7a2d0b19b97eb621d8ff2c8df30c188f405ff
alinzh <siberiangodness@mail.ru>
2024-03-13 15:32:40 +0000

    Add second input to GCN

--- models/GCN/gcn_regression_model.py
+++ models/GCN/gcn_regression_model.py
@@ -22,45 +22,61 @@ test_size = len(dataset) - train_size
 train_data = torch.utils.data.Subset(dataset, range(train_size))
 test_data = torch.utils.data.Subset(dataset, range(train_size, train_size + test_size))
 
-train_dataloader = DataLoader(train_data, batch_size=512, shuffle=True, num_workers=0)
-test_dataloader = DataLoader(test_data, batch_size=10779, shuffle=False, num_workers=0)
+train_dataloader = DataLoader(train_data, batch_size=524, shuffle=True, num_workers=0)
+test_dataloader = DataLoader(test_data, batch_size=15759, shuffle=False, num_workers=0)
 
 class GCN(torch.nn.Module):
   """Graph Convolutional Network"""
 
   def __init__(self):
       super().__init__()
-      self.conv1 = GCNConv(8, 16)
+      self.conv1 = GCNConv(4, 16)
       self.conv2 = GCNConv(16, 8)
-      self.layer3 = Linear(8, 1)
+      self.layer3 = Linear(4, 8)
+      self.layer4 = Linear(16, 1)
+
+  def forward(self, d):
+      # k, p, c, b - additional properties for total graph (see ml-playground/data_massage/props.json)
+      data, y, p, c, apf, wiener = d
+      p = p.view(-1, 1)
+      c = c.view(-1, 1)
+      apf = apf.view(-1, 1)
+      wiener = wiener.view(-1, 1)
 
 
-  def forward(self, data):
+      additional_data = torch.cat((p, c, apf, wiener), dim=1)
       x, edge_index = data.x, data.edge_index.type(torch.int64)
 
-      x = self.conv1(x, edge_index)
+      # first input
+      x = self.conv1(x[:, :4], edge_index)
       x = F.tanh(x)
-
+      x = F.dropout(x, training=self.training)
       x = self.conv2(x, edge_index)
       x = F.tanh(x)
+
+      # second input
+      add_x = self.layer3(additional_data)
+      add_x = F.tanh(add_x)
+
       x = scatter(x, data.batch, dim=0, reduce='sum')
 
-      x = self.layer3(x)
+      x = self.layer4(torch.cat((x, add_x), dim=1))
       return x
 
-device = torch.device('mps')
+device = torch.device('cpu')
 model = GCN()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
 loss_list = []
 
 model.train()
-for epoch in tqdm(range(50)):
+for epoch in tqdm(range(150)):
     mean_loss = 0
     cnt = 0
-    for data, y in train_dataloader:
+    for data, y, p, c, apf, wiener in train_dataloader:
+        d = [data, y, p, c, apf, wiener]
         cnt += 1
         optimizer.zero_grad()
-        out = model(data)
+        out = model(d)
         loss = F.mse_loss(out, y)
         loss.backward()
         optimizer.step()
@@ -68,6 +84,11 @@ for epoch in tqdm(range(50)):
     writer.add_scalar('Loss/train', mean_loss/cnt, epoch)
     loss_list.append(mean_loss)
     print(f'--------Mean loss for epoch {epoch} is {mean_loss/cnt}--------')
+    if epoch % 10 == 0:
+        torch.save(
+            model.state_dict(),
+            f'/root/projects/ml-playground/models/GCN/weights/weights000005_01.pth'
+        )
 
 model.eval()
 total_loss = 0
@@ -76,9 +97,10 @@ num_samples = 0
 with torch.no_grad():
     mean_loss = 0
     cnt = 0
-    for data, y in test_dataloader:
+    for data, y, p, c, apf, wiener in test_dataloader:
+        d = [data, y, p, c, apf, wiener]
         cnt += 1
-        pred = model(data)
+        pred = model(d)
         loss = F.mse_loss(pred, y)
 
         total_loss += loss.item() * data.num_graphs
@@ -93,7 +115,7 @@ with torch.no_grad():
 mse = total_loss / num_samples
 torch.save(
     model.state_dict(),
-    f'/root/projects/ml-playground/models/GCN/weights/weights000001_01.pth'
+    f'/root/projects/ml-playground/models/GCN/weights/weights000005_01.pth'
 )
 writer.close()
 
