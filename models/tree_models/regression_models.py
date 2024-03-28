a6f5afc61968cfeeaf2d8734b29949fdb5f30cdd
alinzh <siberiangodness@mail.ru>
2024-03-28 11:43:39 +0000

    Update dataset for trees model and trees model

--- models/tree_models/regression_models.py
+++ models/tree_models/regression_models.py
@@ -1,64 +1,79 @@
+"""
+Any of the below regression models can be used for predicting Seebeck Coefficient values of binary compounds.
+"""
 import turicreate as tc
+from turicreate import SFrame
 from torchmetrics import MeanAbsoluteError
 from torcheval.metrics import R2Score
 import torch
-from data_massage.seebeck_coefficient_and_structure.data_prepearing_for_decision_tree import data_prepearing_from_file
+import pandas as pd
 
 mean_absolute_error = MeanAbsoluteError()
 metric = R2Score()
 
-total = data_prepearing_from_file(
-    "/data_massage/seebeck_coefficient_and_structure/data/I_C_PEER_INITIO.xlsx"
+# Crystal in vectors format
+total = pd.read_csv(
+    '/root/projects/ml-playground/data_massage/seebeck_coefficient_and_structure/data/26_3/rep_vectors_str_200.csv'
 )
-
-# Any of the below regression models can be used for predicting Seebeck Coefficient values of binary compounds.
+seebeck = pd.read_csv(
+    '/root/projects/ml-playground/data_massage/seebeck_coefficient_and_structure/data/26_3/seebeck_200.csv'
+)
+total = SFrame(pd.concat([seebeck["Seebeck coefficient"], total], axis=1))
+features = ["atom", "distance"]
 
 # LINEAR REGRESSION MODEL
-# if you don't have "I" ans "C" (see ml-playground/data_massage/props.json), remove it
+train_r, test_r = total.random_split(0.9)
 model_linear = tc.linear_regression.create(
-    total, target="Seebeck coefficient", features=["APF", "Wiener", "I", "C"]
+    train_r, target="Seebeck coefficient", features=features
 )
 coefficients_linear = model_linear.coefficients
-predictions_linear = model_linear.predict(total)
-results_linear = model_linear.evaluate(total)
+predictions_linear = model_linear.predict(test_r)
+results_linear = model_linear.evaluate(test_r)
+metric.update(torch.tensor(predictions_linear), torch.tensor(test_r["Seebeck coefficient"]))
+r2_res = metric.compute()
+mean_absolute_error.update(torch.tensor(predictions_linear), torch.tensor(test_r["Seebeck coefficient"]))
+mae_result = mean_absolute_error.compute()
 model_linear.summary()
 
 # DECISION TREE MODEL
 train_d, test_d = total.random_split(0.9)
 model_decision = tc.decision_tree_regression.create(
-    train_d, target="Seebeck coefficient", features=["APF", "Wiener", "I", "C"]
+    train_d, target="Seebeck coefficient", features=features
 )
 predictions_decision = model_decision.predict(test_d)
 results_decision = model_decision.evaluate(test_d)
 featureimp_decision = model_decision.get_feature_importance()
 metric.update(torch.tensor(predictions_decision), torch.tensor(test_d["Seebeck coefficient"]))
+r2_res = metric.compute()
 mean_absolute_error.update(torch.tensor(predictions_decision), torch.tensor(test_d["Seebeck coefficient"]))
-mae_result1 = mean_absolute_error.compute()
+mae_result = mean_absolute_error.compute()
 model_decision.summary()
 
 # BOOSTED TREES MODEL
 train_b, test_b = total.random_split(0.9)
 model_boosted = tc.boosted_trees_regression.create(
-    train_b, target="Seebeck coefficient", features=["APF", "Wiener", "I", "C"]
+    train_b, target="Seebeck coefficient", features=features
 )
 predictions_boosted = model_boosted.predict(test_b)
 results_boosted = model_boosted.evaluate(test_b)
 featureboosted = model_boosted.get_feature_importance()
 metric.update(torch.tensor(predictions_boosted), torch.tensor(test_b["Seebeck coefficient"]))
+r2_res = metric.compute()
 mean_absolute_error.update(torch.tensor(predictions_boosted), torch.tensor(test_b["Seebeck coefficient"]))
-mae_result2 = mean_absolute_error.compute()
+mae_result = mean_absolute_error.compute()
 model_boosted.summary()
 
 metric = R2Score()
 # RANDOM FOREST MODEL
 train_r, test_r = total.random_split(0.9)
 model_random = tc.random_forest_regression.create(
-    train_r, target="Seebeck coefficient", features=["APF", "Wiener", "I", "C"]
+    train_r, target="Seebeck coefficient", features=features
 )
 predictions_random = model_random.predict(test_r)
 results_random = model_random.evaluate(test_r)
 featureimp_random = model_random.get_feature_importance()
 metric.update(torch.tensor(predictions_random), torch.tensor(test_r["Seebeck coefficient"]))
+r2_res = metric.compute()
 mean_absolute_error.update(torch.tensor(predictions_random), torch.tensor(test_r["Seebeck coefficient"]))
-mae_result3 = mean_absolute_error.compute()
+mae_result = mean_absolute_error.compute()
 model_random.summary()
