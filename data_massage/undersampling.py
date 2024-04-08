4570407d0313f3491e00ede66c71a400f749b42d
alinzh <siberiangodness@mail.ru>
2024-04-08 18:58:09 +0000

    Moved file

--- data_massage/undersampling.py
+++ data_massage/undersampling.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 str = pd.read_csv(
-        '/Users/alina/PycharmProjects/ml-playground/data_massage/seebeck_coefficient_and_structure/data/01_04/vectors_str_200.csv'
+    '/root/projects/ml-playground/data_massage/seebeck_coefficient_and_structure/data/01_04/vectors_str_200.csv'
 )
 seebeck = pd.read_csv(
-    '/Users/alina/PycharmProjects/ml-playground/data_massage/seebeck_coefficient_and_structure/data/01_04/rep_seebeck_200.csv'
+    '/root/projects/ml-playground/data_massage/seebeck_coefficient_and_structure/data//01_04/rep_seebeck_200.csv'
 )
 data = pd.concat([seebeck["Seebeck coefficient"], str], axis=1).values.tolist()
 
@@ -31,10 +31,10 @@ for row in sorted(data):
 
 
 pd.DataFrame(new_list_seeb, columns=['Seebeck coefficient']).to_csv(
-    '/Users/alina/PycharmProjects/ml-playground/data_massage/seebeck_coefficient_and_structure/data/01_04/under_seeb_200.csv',
+    '/root/projects/ml-playground/data_massage/seebeck_coefficient_and_structure/data/01_04/under_seeb_200.csv',
     index=False
 )
 pd.DataFrame(new_list_str, columns=['atom', 'distance']).to_csv(
-    '/Users/alina/PycharmProjects/ml-playground/data_massage/seebeck_coefficient_and_structure/data/01_04/under_str_200.csv',
+    '/root/projects/ml-playground/data_massage/seebeck_coefficient_and_structure/data/01_04/under_str_200.csv',
     index=False
 )
