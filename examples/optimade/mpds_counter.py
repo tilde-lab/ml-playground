ff9f2e91bd380a3ad74f373684982822549112bb
alinazh <siberiangodness@mail.ru>
2024-05-17 02:03:36 +0300

    Update example

--- examples/optimade/mpds_counter.py
+++ examples/optimade/mpds_counter.py
@@ -16,41 +16,40 @@ for el in els:
 
 answers = [el for el, _ in groupby(answers)]
 
-for idx in range(len(groups)):
-    # elements HAS ANY "C","Si","Ge","Sn","Pb"
-    if idx == 0:
-        groups[idx] += len(answers)
-    # elements HAS ANY "C",
-    # "Si","Ge","Sn","Pb" AND nelements=2
-    elif idx == 1:
-        for row in answers:
-            num_els = 0
-            formula = row[1]
-            formula_split = re.findall(r'[A-Z]?[^A-Z]*', formula)[:-1]
-            for e in formula_split:
-                num = re.findall(r'\d+', e)
-                if num != [] :
-                    num_els += int(num[0])
-                else:
-                    num_els += 1
-            if num_els == 2:
-                groups[idx] += 1
-    # HAS ANY "C",
-    # "Si","Ge","Sn" AND NOT elements HAS "Pb" AND
-    # elements LENGTH 3
-    else:
-        for row in answers:
-            num_els = 0
-            formula = row[1]
-            formula_split = re.findall(r'[A-Z]?[^A-Z]*', formula)[:-1]
-            for e in formula_split:
-                num = re.findall(r'\d+', e)
-                if num != [] and 'Pb' not in formula:
-                    num_els += int(num[0])
-                elif 'Pb' not in formula:
-                    num_els += 1
-            if num_els == 3:
-                groups[idx] += 1
+# elements HAS ANY "C","Si","Ge","Sn","Pb"
+groups[0] += len(answers)
+
+# elements HAS ANY "C",
+# "Si","Ge","Sn","Pb" AND nelements=2
+for row in answers:
+    num_els = 0
+    formula = row[1]
+    formula_split = re.findall(r'[A-Z]?[^A-Z]*', formula)[:-1]
+    for e in formula_split:
+        num = re.findall(r'\d+', e)
+        if num != [] :
+            num_els += int(num[0])
+        else:
+            num_els += 1
+    if num_els == 2:
+        groups[1] += 1
+
+# HAS ANY "C",
+# "Si","Ge","Sn" AND NOT elements HAS "Pb" AND
+# elements LENGTH 3
+for row in answers:
+    num_els = 0
+    formula = row[1]
+    formula_split = re.findall(r'[A-Z]?[^A-Z]*', formula)[:-1]
+    for e in formula_split:
+        num = re.findall(r'\d+', e)
+        if num != [] and 'Pb' not in formula:
+            num_els += int(num[0])
+        elif 'Pb' not in formula:
+            num_els += 1
+    if num_els == 3:
+        groups[2] += 1
 
 print(groups)
+
 
